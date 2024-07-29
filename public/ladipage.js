85d05deae24446b6467758b6519a1b3f8de5df3a
ttnghanuel <92569088+ttnghanuel@users.noreply.github.com>
2024-07-30 00:33:15 +0700

    Update ladipage.js

--- public/ladipage.js
+++ public/ladipage.js
@@ -5,8 +5,9 @@ async function submitForm() {
         const response = await fetch('https://m-scan-payos.onrender.com/create_payment_link', { method: 'POST' });
         const payment = await response.json();
         console.log("Payment data:", payment);
+        window.location.href = payment.checkoutUrl;
 
-        window.open(payment.checkoutUrl);
+
     } catch (error) {
         console.error('Error:', error);
     }
