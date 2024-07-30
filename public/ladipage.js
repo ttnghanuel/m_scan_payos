cb4ab2f7dcd96de4221df227930816ba6872a255
ttnghanuel <92569088+ttnghanuel@users.noreply.github.com>
2024-07-30 15:34:19 +0700

    Update ladipage.js

--- public/ladipage.js
+++ public/ladipage.js
@@ -1,5 +1,13 @@
 document.getElementById("btnClick").addEventListener("click", function() {
         window.location.replace("https://payos-msca-duthi.onrender.com/payment.html");
       });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 
