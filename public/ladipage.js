95f64f57ab24621d6416f4a13e4ec7c4cd0c5305
ttnghanuel <92569088+ttnghanuel@users.noreply.github.com>
2024-07-30 16:22:14 +0700

    Update ladipage.js

--- public/ladipage.js
+++ public/ladipage.js
@@ -2,5 +2,8 @@ document.getElementById("btnClick").addEventListener("click", function() {
         window.location.replace("https://payos-msca-duthi.onrender.com/payment.html");
       });
 
-
+ document.getElementById('sendButton').addEventListener('click', function() {
+        this.textContent = 'Đã gửi email thành công';
+        this.disabled = true; // Optional: Disable the button to prevent further clicks
+      });
 
