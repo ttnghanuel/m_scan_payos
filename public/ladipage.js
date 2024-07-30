25919de8637bb42abc425b104dcbe86e4ecde341
ttnghanuel <92569088+ttnghanuel@users.noreply.github.com>
2024-07-30 12:55:29 +0700

    Update ladipage.js

--- public/ladipage.js
+++ public/ladipage.js
@@ -2,19 +2,4 @@ document.getElementById("btnClick").addEventListener("click", function() {
         window.location.replace("https://payos-msca-duthi.onrender.com/payment.html");
       });
 
-document.getElementById('btnPayment').addEventListener('click', submitForm);
-
-async function submitForm() {
-    try {
-        const response = await fetch('https://m-scan-payos.onrender.com/create_payment_link', { method: 'POST' });
-        const payment = await response.json();
-        console.log("Payment data:", payment);
-        window.location.replace(payment.checkoutUrl);
-
-
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
 
